c807fd8a9dd719b989914fe17a3858d10fbb0315
AlanYnov <alan.philipiert@ynov.com>
2020-10-22 14:01:36 +0200

    v2 css test à implanter

---
+++ importCssV/public/js/main.js
@@ -0,0 +1,38 @@
+// $('.btn').click(function(){
+//     $('#global').css('display', 'flex');
+//     $('#mainCtn').css('display', 'none');
+// })
+
+$('#notification').click(function(){
+    if($('.active')[0]){
+        $('#notification').attr('src', 'img/bell.png')
+        $('#notification').removeClass('active')
+    } else{
+        $('#notification').attr('src', 'img/bell1.png')
+        $('#notification').addClass('active')
+    }
+})
+
+$(".emojiFrame").click(function(){
+    let target = $("#m");
+    let smile = $(this).text();
+    myText = target.val() + smile
+    target.val( myText );
+})
+
+$(document).click(function() {
+    $('.emojiPicker').css('display', 'none');
+    $('.emojiPicker').addClass('hideEmoji');
+});
+
+$('#smileBtn').click(function(event){
+    if($('.hideEmoji')[0]){
+        $('.emojiPicker').css('display', 'flex');
+        $('.emojiPicker').removeClass('hideEmoji');
+    }
+    else {
+        $('.emojiPicker').css('display', 'none');
+        $('.emojiPicker').addClass('hideEmoji');
+    }
+    event.stopPropagation();
+})
---
+++ importCssV/server.js
@@ -0,0 +1,121 @@
+
+//initialisation des variables
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public')); //on utilise le dossier public
+
+const userList = [];
+const msgEnvoiGeneral = [];
+const msgEnvoiPrivate = [];
+const returnGeneral = [];
+
+io.on('connection', function(socket){ //nouvelle connexion
+	console.log('Nouvelle connexion');
+  
+	socket.on('chat message', function(msg) { //lorsqu'on envoit un message
+		let userName = '';
+		let msgEnvoi = '';
+		for(let i = 0; i < userList.length; i++){
+			if((userList[i].id) == (socket.id)){ //on cherche le nom de l'expediteur dans notre tableau des users
+				userName = userList[i].user;
+				msgEnvoi = {id:socket.id,message:msg,expediteur:userName};
+				msgEnvoiGeneral.push(msgEnvoi); //renvoit en Json les infos du message
+				if(msgEnvoiGeneral.length > 20){ //si il a plus de 20 messages dans le chat général
+					msgEnvoiGeneral.shift();
+				}
+				io.emit('chat message', msgEnvoi, msgEnvoiGeneral);
+			}
+		}
+		//console.log(msgEnvoiGeneral);
+	});
+  
+	socket.on('user', (user) => { //ajout d'un nouvelle utilisateur
+		let connexion = '';
+		if(!userList.find(user => user.id === socket.id)){
+			userList.push({id:socket.id,user:user});
+			for(let i = 0; i < userList.length; i++){
+				if((userList[i].id) == (socket.id)){ //on cherche le nom de l'expediteur dans notre tableau des users
+					userName = userList[i].user;
+					connexion = {id:'serveur',message:'est entré(e) dans le salon',expediteur:userName,connected:socket.id};
+					msgEnvoiGeneral.push(connexion); //renvoit en Json les infos du message
+					if(msgEnvoiGeneral.length > 20){ //si il a plus de 20 messages dans le chat général
+						msgEnvoiGeneral.shift();
+					}
+					//io.emit('is connected', connexion);
+				}
+			}
+		}
+		console.log(userList);
+		io.emit('new user', userList);
+		io.emit('co affiche', connexion);
+	});
+  
+	socket.on('private message', function(message,idPrivate) { //pour envoyer un message privé
+		let userName = '';
+		for(let i = 0; i < userList.length; i++){
+			if((userList[i].id) == (socket.id)){ //on cherche le nom de l'expediteur dans notre tableau des users
+				userName = userList[i].user;
+				msgEnvoiPrivate.push({id:socket.id,message:message,expediteur:userName,destinataire:idPrivate}); //renvoit en Json les infos du message
+				io.emit('private message', msgEnvoiPrivate);
+				//io.to(idPrivate).emit(msgEnvoiPrivate);
+			}
+		}
+		console.log(msgEnvoiPrivate);
+	});
+	
+	socket.on('enter private message', function(click) { //pour arriver sur une conv privée
+		if(click == true){
+			io.emit('enter private message', msgEnvoiPrivate); //on renvoit le tableau des messages privés
+		}
+		console.log(msgEnvoiPrivate);
+	});
+	
+	socket.on('typing', typing => { //pour renvoyer si le user tape ou non
+		let userName = '';
+		for(let i = 0; i < userList.length; i++){
+			if((userList[i].id) == (socket.id)){ //on cherche le nom de l'expediteur dans notre tableau des users
+				userName = userList[i].user;
+			}
+		}
+		//on envoit le tableau au client
+		const barre = [];
+		barre.push({user:userName,typing:typing,id:socket.id});
+		io.emit('display', barre);
+		//console.log(barre);
+	});
+	
+	socket.on('deconnexion', userName => { //pour renvoyer si se déconnecte
+		let deconnexion = '';
+		for(let i = 0; i < userList.length; i++){
+			if((userList[i].id) == (socket.id)){ //on cherche le nom de l'expediteur dans notre tableau des users
+				let supprimeUser = userList.splice(i, 1);
+				deconnexion = {id:'serveur',message:'a quitté le salon',expediteur:userName};
+				msgEnvoiGeneral.push(deconnexion); //renvoit en Json les infos du message
+				if(msgEnvoiGeneral.length > 20){ //si il a plus de 20 messages dans le chat général
+					msgEnvoiGeneral.shift();
+				}
+			}
+		}
+		io.emit('deco affiche', deconnexion);
+		io.emit('deconnexion', userList); //on renvoit le nouveau tableau des users
+		console.log(userList);
+		console.log(deconnexion);
+	});
+	
+	socket.on('general', click => { //pour renvoyer les messages généraux
+		if(click == true){
+			returnGeneral.push({id:socket.id});
+			io.emit('general', msgEnvoiGeneral, returnGeneral); //on renvoit le tableau des messages general
+		}
+		console.log(msgEnvoiGeneral);
+		console.log(returnGeneral);
+	});
+});
+
+http.listen(port, function(){ //vérifier sur quel port écoute le serveur
+	console.log('listening on *:' + port);
+});
